c56cd89131589a275b5661f835ca6408017bf658
uhalpern <64875604+uhalpern@users.noreply.github.com>
2025-01-15 09:50:08 -1000

    implemented helper functions for ingesting
    implemented helper functions for ingesting

--- src/export_excel.py
+++ src/export_excel.py
@@ -12,7 +12,13 @@ Date: 2024-12-24
 
 import sqlite3
 import os
+import openpyxl.cell
+import openpyxl.workbook
+import openpyxl.worksheet
+import openpyxl.worksheet.worksheet
 import pandas as pd
+import openpyxl
+from openpyxl import load_workbook
 
 
 
@@ -116,14 +122,14 @@ def create_sheet(final_df: pd.DataFrame, sheet_name: str = "Sheet1",
     Returns:
         full_path (str): Path where spreadsheet was saved
     """
-    
+   
     # Create directory if it does not already exist
     if not os.path.exists(file_path):
         os.mkdir(file_path)
 
     # Combine path and file name
     full_path = os.path.join(file_path, file_name)
-    
+  
     # Error handling for sheet already existing
     if os.path.exists(full_path):
         raise FileExistsError(f'The file already exists: {full_path}')
@@ -133,3 +139,78 @@ def create_sheet(final_df: pd.DataFrame, sheet_name: str = "Sheet1",
     print(f'Sheet saved to {file_name} with sheet name: {sheet_name} at {full_path}') 
 
     return full_path
+
+
+def process_row(sheet: openpyxl.worksheet.worksheet, dataframe_row: list, sheet_row: tuple,
+                validation_format_dict: dict) -> None:
+    """
+    Ingests a single dataframe row into a single spreadsheet row in the provided sheet.
+
+    Args:
+        sheet: Sheet object that dataframe is ingesting into
+        dataframe_row (list): preprocessed dataframe row to ingest into sheet_row
+        sheet_row (tuple): tuple of cells representing a spreadsheet row
+
+    """
+
+    for cell in sheet_row:
+        
+        # Prevent overwriting of cells that have formulas
+        if cell.value is None:
+
+            # Get value from dataframe by indexing with the cell column number
+            val_to_insert = dataframe_row[cell.column]
+
+            # Insert the Value
+            cell.value = val_to_insert
+
+            # Add formatting
+            get_format(sheet, cell, validation_format_dict)
+
+
+def get_format(sheet: openpyxl.worksheet.worksheet, cell: openpyxl.cell.cell.Cell, 
+               validation_format_dict: dict) -> None:
+    """
+    Returns the format string of a cell from the validation_format_dict
+
+    Args:
+        sheet: (openpyxl.worksheet.worksheet): spreadsheet object for accessing header cell
+        cell (openpyxl.cell.cell.Cell): cell to get formatting for
+        validation_format_dict (dict): dictionary that holds formatting for each column
+
+    """
+
+    # Get the columns header from the sheet
+    column_letter = cell.column_letter
+    header = sheet[f"{column_letter}1"]
+
+    # Look for the header in the validation_format_dict
+    format_rules = validation_format_dict.get(header)
+
+    # Apply style formatting if the header and style_format field is found
+    if format_rules is not None:
+        style_format = validation_format_dict[header].get("style_format")
+
+        if style_format is not None:
+            cell.number_format = style_format
+
+
+def insert_into_template(final_df: pd.DataFrame, validation_format_dict: dict, workbook_name: str = "CTS_Insert_Example.xlsx"):
+    """
+    Inserts data into the template spreadsheet using data from the final_df row by row
+
+    Args:
+        final_df (pandas.dataframe): dataframe which holds transformed data from SQL query
+        validation_format_dict (dict): dictionary that holds formatting for each column
+        workbook_name (str): name of workbook that will be saved after data is ingested
+
+    """
+
+    parent_dir = os.path.abspath(os.path.join(os.getcwd()))
+    sheets_directory = os.path.join(parent_dir, 'generated_sheets')
+
+    template_file_path = os.path.join(parent_dir, 'CTS_Example_Template')
+
+    workbook = load_workbook()
+
+    pass
